cc666a2013b705b006fa0b66519cf4e7fd65ed6f
Lucas <lucas.encrenaz@yahoo.fr>
2020-09-18 14:40:12 +0200

    Add text in php and style in css

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 <html>
   <head>
     <title>Contrôle de température</title>
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <h1>Contrôle de température</h1>
+    <h1 class="titrePrincipal">Contrôle de température</h1>
+    <div class="temperature">
+    <p>Température du joueur : </p>
+    <p>Joueur apte à jouer : </p>
+    </div>
   </body>
 </html>
