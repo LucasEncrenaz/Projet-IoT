5e2250a58328d165075dfe9a9f1a2193381e12d7
Lucas <lucas.encrenaz@yahoo.fr>
2020-09-28 13:31:28 +0200

    Liste + JS
    Ajout d'une liste déroulante récupérant les noms depuis le JSON et affichant la div correspondante lors du clic sur la liste

--- index.php
+++ index.php
@@ -4,17 +4,39 @@
   <head>
     <title>Contrôle de température</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" href="logo.png" />
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+    <script src="script.js"></script>
   </head>
   <body>
     <h1 class="titrePrincipal">Contrôle de température</h1>
     <div class="temperature">
-    <?php 
-    $json = json_decode (file_get_contents('valeurs.json'), true);
-    ?>
-    <p class="label">Nom du joueur :</p> <p class="input"><?php echo $json['name']; ?> </p>
-    <p class="label">Date de naissance joueur :</p> <p class="input"><?php echo $json['dateNaissance']; ?> </p>
-    <p class="label">Température du joueur :</p> <p class="input"><?php echo $json['temperature']; ?>°C </p>
-    <p class="label">Joueur apte à jouer :</p>  <p class="input"><?php apteAjouer($json['temperature']); ?> </p>
+      <?php 
+      $json = file_get_contents("valeurs.json");
+      $content = json_decode($json, true);
+      ?> <select class="maliste" > <?php
+      foreach ($content['data'] as $mydata)
+      {
+        ?>
+        <option value ="<?php echo $mydata['id']; ?>" class="listeOption">
+          <?php echo $mydata['name']; ?>
+        </option>
+        <?php
+      }
+      ?>
+    </select>
+    <?php
+    foreach ($content['data'] as $mydata)
+      {
+        ?>
+        <div class="<?php echo $mydata['id']; ?> msg">
+          <p>Nom : <?php echo $mydata['name']; ?></p>
+          <p>Date de naissance : <?php echo $mydata['dateNaissance']; ?></p>
+          <p>Température : <?php echo $mydata['temperature']; ?>°C</p>
+      </div>
+        <?php
+      }
+      ?>
     </div>
   </body>
 </html>
