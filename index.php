9a717da0fcab9612ac2b949f1564f68dbefee8ca
Lucas <lucas.encrenaz@yahoo.fr>
2020-09-28 16:35:41 +0200

    last commit

--- index.php
+++ index.php
@@ -43,45 +43,65 @@
     </div>
     <script type="text/javascript">
 
-    var clientId = 'a:OrgId:projetiot';
-    var client = new  Paho.MQTT.Client("y15qu1.messaging.internetofthings.ibmcloud.com", 8883, clientId);
+    var clientId = 'd:y15qu1:Pycom:4325';
+    var client = new  Paho.MQTT.Client("y15qu1.messaging.internetofthings.ibmcloud.com", 443, clientId);
+
+    
 
     //Gets  called if the websocket/mqtt connection gets disconnected for any reason
-    client.onConnectionLost = function (responseObject) {
+    client.onConnectionFailure = function (responseObject) {
         //Depending on your scenario you could implement a reconnect logic here
         alert("connection lost: " + responseObject.errorMessage);
     };
 
-    //Gets called whenever you receive a message for your subscriptions
-    client.onMessageArrived = function (message) {
-        //Do something with the push message you received
-        $('#messages').append('<span>Topic: ' + message.destinationName + '  | ' + message.payloadString + '</span><br/>');
+    client.onConnectionSuccess = function (responseObject) {
+        //Depending on your scenario you could implement a reconnect logic here
+        alert("connection win: " + responseObject.errorMessage);
     };
 
-    //Connect Options
-    var options = {
-        userName: "a-y15qu1-xelz46my3f",
-        password: "+qNATNg3Z8cW0Ma9Vq",
-        timeout: 3,
-        //Gets Called if the connection has sucessfully been established
-        onSuccess: function () {
-            alert("Connected");    	 
-        },
-        //Gets Called if the connection could not be established
-        onFailure: function (message) {
-            alert("Connection failed: " + message.errorMessage);
-        }
-    };
+    window.client.connect({
+            onSuccess: function () {
+                alert("Connected");    	 
+            },
+            //Gets Called if the connection could not be established
+            onFailure: function (message) {
+                alert("Connection failed: " + message.errorMessage);
+            },
+            userName: "a-y15qu1-xelz46my3f",
+            password: "+NATNg3Z8cW0Ma9Vq",
+            useSSL: true
+    });
 
-    //Creates a new Messaging.Message Object and sends it to the HiveMQ MQTT Broker
-    var publish = function (payload, topic, qos) {
-        //Send your message (also possible to serialize it as JSON or protobuf or just use a string, no limitations)
-        var message = new Messaging.Message(payload);
-        message.destinationName = topic;
-        message.qos = qos;
-        client.send(message);
-    }
-    client.connect(options);
+    // //Gets called whenever you receive a message for your subscriptions
+    // client.onMessageArrived = function (message) {
+    //     //Do something with the push message you received
+    //     $('#messages').append('<span>Topic: ' + message.destinationName + '  | ' + message.payloadString + '</span><br/>');
+    // };
+
+    // //Connect Options
+    // var options = {
+    //     userName: "a-y15qu1-xelz46my3f",
+    //     password: "+qNATNg3Z8cW0Ma9Vq",
+    //     timeout: 3,
+    //     //Gets Called if the connection has sucessfully been established
+    //     onSuccess: function () {
+    //         alert("Connected");    	 
+    //     },
+    //     //Gets Called if the connection could not be established
+    //     onFailure: function (message) {
+    //         alert("Connection failed: " + message.errorMessage);
+    //     }
+    // };
+
+    // //Creates a new Messaging.Message Object and sends it to the HiveMQ MQTT Broker
+    // var publish = function (payload, topic, qos) {
+    //     //Send your message (also possible to serialize it as JSON or protobuf or just use a string, no limitations)
+    //     var message = new Messaging.Message(payload);
+    //     message.destinationName = topic;
+    //     message.qos = qos;
+    //     client.send(message);
+    // }
+    // client.connect(options);
   </script>
   </body>
 </html>
